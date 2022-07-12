22f98f570768d993a73c14b33ed0de71dec20052
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-07-12 10:12:10 +0300

    Add reducers and actions

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,31 +1,34 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 const inState = [];
-const bookReducer = (state = inState, action) => {
+const ADD_BOOK = './books/ADD_BOOK';
+const REMOVE_BOOK = './books/REMOVE_BOOK';
+
+const bookSlice = (state = inState, action) => {
   switch (action.type) {
-    case 'ADD_Book': {
+    case ADD_BOOK: {
       const newBook = {
+        id: Math.floor(Math.random() * 1000),
         title: action.book.title,
-        id: action.book.title,
         author: action.book.author,
+        category: action.book.category,
       };
-    }
       return [...state, newBook];
-    case 'Remove': {
-      const remove = state.filter((book) => book.id !== action.id);
+    }
+    case REMOVE_BOOK: {
+      const remove = state.filter((book) => book[0] !== action.id);
       return remove;
     }
-    default: return state;
+    default:
+      return state;
   }
 };
-
 export const addBookFunc = (book) => ({
-  type: 'ADD_BOOK',
+  type: ADD_BOOK,
   book,
 });
+
 export const removeBookFunc = (id) => ({
-  type: 'REMOVE_BOOK',
+  type: REMOVE_BOOK,
   id,
 });
 
-export default bookReducer;
+export default bookSlice.reducer;
